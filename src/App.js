da879304e557d7de0d494eab9478431ea7a6b7bd
Abhishek Roy <abhishekroy9824@gmail.com>
2022-11-07 17:03:48 +0530

    Navbar, Homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import "./App.css";
+import {
+  Cryptocurrencies,
+  CryptoDetails,
+  Exchanges,
+  Homepage,
+  Navbar,
+  News,
+} from "./components";
 
 function App() {
   return (
-    <>
-      <div>hello world</div>
-    </>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Crypto Village
+            <br />
+            All rights reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+import icon from "../images/cryptocurrency.png";
+import Item from "antd/lib/list/Item";
+
+const navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Crypto Village</Link>
+        </Typography.Title>
+      </div>
+      <Menu theme="dark">
+        <Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Item>
+        <Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        </Item>
+        <Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Item>
+        <Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Item>
+      </Menu>
+    </div>
+  );
+};
+
+export default navbar;
